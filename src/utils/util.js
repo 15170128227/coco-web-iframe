11a9de3bfc88f78f32639cddcac2b0f96acb6996
林科 <linke@cocosurprise.com>
2017-12-08 19:08:56 +0800

    更新了axios序列化，解决post请求失败问题

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import axios from 'axios'
 
 axios.interceptors.request.use(function (config) {
-  let params = new URLSearchParams()
-  Object.keys(config.data).forEach(v => {
-    params.append(v, config.data[v])
-  })
-  config.data = params
+  if (config.method === 'post') {
+    let params = new URLSearchParams()
+    Object.keys(config.data).forEach(v => {
+      params.append(v, config.data[v])
+    })
+    config.data = params
+  }
   // Do something before request is sent
   return config
 }, function (error) {
