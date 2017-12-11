bfaa48d1b4339077be50c5e4dbf29115e977a874
heliuxia <heliuxia@cocosurprise.com>
2017-12-11 09:52:45 +0800

    1.文档中心

--- src/pages/document/document.vue
+++ src/pages/document/document.vue
@@ -494,7 +494,7 @@
           overflow hidden
           .one-columns
             a
-              padding 20px
+              padding 21px
           li
             a
               /*padding 12px 0*/
@@ -505,6 +505,7 @@
               background-color #555
               display block
               a
+                padding 21px
                 font-size 14px
                 font-weight normal
             .twoColumn
